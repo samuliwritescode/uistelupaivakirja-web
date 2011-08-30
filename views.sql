335e4b33a4ca0ec6c07783d872055c94b2e5bde0
Samuli Penttila <samuli.penttila@gmail.com>
2011-08-30 16:21:44 +0300

    Spinneritems view

--- views.sql
+++ views.sql
@@ -1,5 +1,10 @@
 drop view if exists spinneritem_view;
 create view spinneritem_view as
-select fish_species, fish_method, fish_getter from event
-group by fish_species, fish_method, fish_getter
+select distinct 'fish_species' as keyname, fish_species as value, user_id from event join trollingobject on(trolling_id=trollingobject.id) join collection on(collection_id=collection.id) where fish_species is not null
+union
+select distinct 'fish_method' as keyname, fish_method as value, user_id from event join trollingobject on(trolling_id=trollingobject.id) join collection on(collection_id=collection.id) where fish_method is not null
+union
+select distinct 'fish_getter' as keyname, fish_getter as value, user_id from event join trollingobject on(trolling_id=trollingobject.id) join collection on(collection_id=collection.id) where fish_getter is not null
+order by keyname, value
 ;
+
